3542f7dcbea8bd628356d70bf85aa17f64a5036d
Jo Grimstad <jo@literacyapp.org>
2017-02-24 21:06:00 +0300

    Updated Launcher icon

--- app/src/main/java/org/literacyapp/imagepicker/MainActivity.java
+++ app/src/main/java/org/literacyapp/imagepicker/MainActivity.java
@@ -2,11 +2,13 @@ package org.literacyapp.imagepicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getClass().getName(), "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
