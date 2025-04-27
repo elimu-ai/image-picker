3c5e7c4f7b436528b0d5cf3c8f6e34645d63e3ab
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-04-27 16:40:20 +0700

    test: postfix .debug to package name

--- app/src/androidTest/java/ai/elimu/imagepicker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ai/elimu/imagepicker/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ai.elimu.imagepicker", appContext.getPackageName());
+        assertEquals("ai.elimu.imagepicker.debug", appContext.getPackageName());
     }
 }
